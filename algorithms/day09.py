76baa2ec426e2bcf00c32278cb8a367c799129a3
kyueunQ <35055662+kyueunQ@users.noreply.github.com>
2018-05-30 01:42:30 +0900

    (정렬) 삽입 정렬

--- algorithms/day09.py
+++ algorithms/day09.py
@@ -1,15 +1,16 @@
-# 삽입 정렬
+# 삽입정렬
 
+# 첫번째 방법
 # 1. 위치 찾기
 def sort_insert_ind(a, x):
-
-  for i in range(0, len(a)):
+  n = len(a)
+  for i in range(0, n):
     if x < a[i]:
       # 17(i번째), 23, 33이 있을 때,
       # 15가 들어간다면 15 < 17
       # 15(i번째), 17, 23, 33 으로 재정렬
       return i
-  return len(a)
+  return n
 
 
 # 2. 새로운 리스트에 순서대로 넣기
@@ -17,8 +18,8 @@ def sort_insert(a):
   new_a = []
   while a:
      x = a.pop(0)
-     ind = sort_insert_ind(a, x)
-     # append() takes exactly one argument
+     ind = sort_insert_ind(new_a, x)
+     # append(): takes exactly one argument
      # new_a.append(ind, x)
      new_a.insert(ind, x)
   return new_a
@@ -26,3 +27,5 @@ def sort_insert(a):
 
 v = [55, 33, 13, 46, 82, 28]
 print(sort_insert(v))
+
+
